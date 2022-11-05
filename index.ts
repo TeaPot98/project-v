084822eb74884670ad8a94ed62a6c2c207cd2065
TeaPot98 <trifautan@gmail.com>
2022-11-05 14:31:02 +0200

    fixed yarn pnp node_modules issue and created example app

--- index.ts
+++ index.ts
@@ -1,3 +1,12 @@
-console.log('Hello world');
+import express from "express";
 
-export {}
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
