37f5805b9ca127877726b6fa255470b7ca99fee1
TeaPot98 <trifautan@gmail.com>
2022-11-24 11:45:55 +0200

    created get all users endpoint and fixed passwordHash leak

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,13 +1,32 @@
 import { Router } from "express";
+import { User } from "models";
+import { Roles } from "types";
 
-import { getUser } from "utils";
+import { ForbiddenError, getUser } from "utils";
 
 export const usersRouter = Router();
 
 usersRouter.get("/logged", async (req, res, next) => {
   try {
     const user = await getUser(req);
-    res.json(user);
+    const { passwordHash, ...userWithoutHash } = JSON.parse(
+      JSON.stringify(user)
+    );
+
+    res.json(userWithoutHash);
+  } catch (err) {
+    next(err);
+  }
+});
+
+usersRouter.get("/", async (req, res, next) => {
+  try {
+    const { role } = await getUser(req);
+    if (role !== Roles.ADMIN)
+      throw new ForbiddenError("Only admins can access the users list");
+
+    const users = await User.find();
+    res.json(users);
   } catch (err) {
     next(err);
   }
